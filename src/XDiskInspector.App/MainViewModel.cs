6c1f3cf60f5a1b46b65719447dead2a1dc359d63
Aix9527 <976936105@qq.com>
2026-09-07 20:04:25 +0800

    fix: import file-system APIs in WPF view model

--- src/XDiskInspector.App/MainViewModel.cs
+++ src/XDiskInspector.App/MainViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Security.Principal;
 using System.Windows;
 using System.Windows.Data;
